ccaab3db851c7543245261ac0c79a1d440016b9a
Eric Goddard <egoddard@memphis.edu>
2015-09-15 16:50:10 -0500

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,22 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VB_NAME = "vagrant-geo"
+CPUS = 1
+MEMORY = "2048"
+
+Vagrant.configure(2) do |config|
+    config.vm.box = "ubuntu/trusty64"
+    config.vm.hostname = VB_NAME
+    config.vm.host_name = VB_NAME
+    config.vm.network "forwarded_port", guest: 8888, host: 8888
+
+    config.vm.provider "virtualbox" do |vb|
+        vb.memory = MEMORY
+        vb.cpus = CPUS
+        vb.gui = false
+        vb.name = VB_NAME
+    end
+
+    config.vm.provision "shell", path:"provision.sh"
+end
---
+++ provision.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Add the UbuntuGIS PPA for more recent packages
+sudo add-apt-repository -y -s ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# Install some of the base packages we'll need
+sudo apt-get -y install build-essential python-pip python3-pip gdal-bin python-gdal python3-gdal python-dev python-setuptools libgdal-dev libgeos-dev git wget python-mpltoolkits.basemap
+
+# Use pip to install the additional python packages we'll need
+yes | sudo pip2 install ipython[notebook] pandas fiona shapely requests sqlalchemy geopandas mplleaflet
+
+# Symlink the /vagrant shared directory to home directory
+if [ ! -L ~/workspace ]; then
+    ln -s /vagrant ~/workspace
+else
+    echo "~/workspace is already linked."
+fi
